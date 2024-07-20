4618e41fbe496239323c72e9aed1f0dc07c25fbe
NickRomanchuk <nickroman@hotmail.com>
2024-07-20 19:48:31 -0400

    Refactor of player card

--- src/components/TeamDisplay/PlayerCard/PlayerCard.js
+++ src/components/TeamDisplay/PlayerCard/PlayerCard.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { playerCardHeight } from "../../../constants/constants";
 import PlayerPopUp from "../PlayerPopUp/PlayerPopUp";
 import "./PlayerCard.css"
+import { Col, Row } from "react-bootstrap";
 
 function PlayerCard({player, length, color, index}) {
     const [showPopUp, setShowPopUp] = useState(false);
@@ -11,21 +12,23 @@ function PlayerCard({player, length, color, index}) {
             <PlayerPopUp showPopUp={showPopUp} setShowPopUp={setShowPopUp} player={player} color={color}/>
             <div className="playerCard" onClick={handleClick} style={{top: `${index * (playerCardHeight / (length-1))}%`, 
                                                 backgroundImage: `linear-gradient(120deg, ${color} 0%, #FFFFFF 100%)`}}>
-                <header className="playerHeader" style={{backgroundColor: `${color}`}}>
-                    {player.firstName[0] + ". " + player.lastName}
+                <header className="d-flex justify-content-center playerHeader" style={{backgroundColor: `${color}`}}>    
+                    <Col className="playerNumber">
+                        {player.number}
+                    </Col>
+                    <Col className="playerTitle" xs={10}>
+                        {player.firstName[0] + ". " + player.lastName}
+                    </Col>
+                    <Col className="playerStatus" >
+                        {player.statusAbbr}
+                    </Col>
                 </header>
                 <section className="playerBody" style={{backgroundImage: `url(${player.headshot})`}}>
-                    <p className="playerNumber">
-                        {player.number}
-                    </p>
                     <p className="playerExperience">
                         {player.exp == "Rookie" ? "Rk" : player.exp.split(" ")[0]}
                     </p>
                     <p className="playerOverall" style={{color: `${color}`}}>
                         {player.overall}
-                    </p>
-                    <p className="playerStatus">
-                        {player.statusAbbr}
                     </p>
                 </section>
             </div>
